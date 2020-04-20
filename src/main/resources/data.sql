41b8b5ec291583ba088cd1106e3f20586ee3dd86
Orkhan098 <orxan-heshimli@bk.ru>
2020-04-21 02:34:32 +0400

    Commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into members values (1,'Orkhan','Hashimli','a','bb','cc','d','male');
 insert into members values (2,'Rahim','Sharifov','a','bb','cc','d','male');
 insert into members values (5,'Parsha','Joarder','a','bb','cc','d','female');
+
